158f9f867328cc14648391d53d2a945d549b6143
Zack Timbol <ttimbol08@gmail.com>
2023-01-11 14:23:08 +0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pushnoti',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
